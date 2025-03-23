cb0bd06422415ab8780fdac1964b51607b7bdb37
MelissaEPFL <melissa.faggella@gmail.com>
2025-03-23 10:58:01 +0100

    update bugs and ingredient grid display in center

--- Game/grid_container.gd
+++ Game/grid_container.gd
@@ -2,19 +2,22 @@ extends GridContainer
 
 var nbr_ingr:int = 4
 
-#children ingredients
-@onready var ingr1 = $Ingredient1
-@onready var ingr2 = $Ingredient2
-@onready var ingr3 = $Ingredient3
-@onready var ingr4 = $Ingredient4
-
 # Array to store the state of each ingredient
 var ingredient_states:Array = ["empty", "empty","empty","empty"]
-var ingredients:Array = [ingr1,ingr2,ingr3,ingr4]
+var ingredients:Array = []
 
 func _ready():
+	
+	#Get children ingredients
+	var ingr1 = get_node("ingredient1")
+	var ingr2 = get_node("ingredient2")
+	var ingr3 = get_node("ingredient3")
+	var ingr4 = get_node("ingredient4")
+	ingredients = [ingr1,ingr2,ingr3,ingr4]	
+	
 	# Initialize empty ingredients
 	for i in range(nbr_ingr):
+		print(ingredients[i])
 		ingredients[i].play("empty")
 
 # Update a specific ingredient in the grid
